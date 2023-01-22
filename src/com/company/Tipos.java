b93e1d8c50817888dc141bb7072ef44155c7b116
Leyci Buelvas <leycibuelvas73@gmail.com>
2023-01-21 22:15:59 -0500

    open bootcamp

---
+++ src/com/company/Tipos.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Tipos {
+
+    public static void main(String[] args) {
+
+        //tipos de datos
+
+        int i = 0;
+        System.out.println(i);
+
+        byte variable = 5;
+        System.out.println(variable);
+
+        short var2= 15;
+        System.out.println(var2);
+
+        long var3 = 2000;
+        System.out.println(var3);
+
+        float var4 = 4.4f;
+        System.out.println(var4);
+
+        double var5 = 5.5d;
+        System.out.println(var5);
+
+        boolean verdadero = true;
+        System.out.println(verdadero);
+
+        String nombre = "Leyci Buelvas";
+        System.out.println(nombre);
+
+        char variable8 = 'k';
+        System.out.println(variable8);
+    }
+}
